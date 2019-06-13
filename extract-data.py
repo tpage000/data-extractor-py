51fb79a7987ee62016d40682ad52b938c84b1a24
Thom Page <thom.page.000@gmail.com>
2019-06-13 19:10:19 -0400

    Add input flag validation

--- extract-data.py
+++ extract-data.py
@@ -11,5 +11,28 @@ load_dotenv()
 # --test    --print/--csv/--count
 # --prod    --count
 
-print('extract data')
+def main():
+    env_flag_list = ['--dev', '--test', '--prod']
+    output_flag_list = ['--print', '--count', '--csv']
+    return (
+        print_args_error('Missing arguments') if len(sys.argv) < 3
+        else print_args_error('Incorrect env arguments') if not sys.argv[1] in env_flag_list
+        else print_args_error('Incorrect output arguments') if not sys.argv[2] in output_flag_list
+        else print_args_error('Cannot print or save prod data') if sys.argv[1] == '--prod' and sys.argv[2] != '--count'
+        else set_connection(sys.argv[1], sys.argv[2]))
 
+def set_connection(env_flag, output_flag):
+    print(env_flag, output_flag)
+
+def print_args_error(message):
+    print(f'{message}. Please supply: ')
+    print('First argument: environment')
+    print('--dev')
+    print('--test')
+    print('--prod')
+    print('Second argument: output format')
+    print('--count')
+    print('--print (not for prod)')
+    print('--csv (not for prod)')
+
+main()
