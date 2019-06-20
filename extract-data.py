0b0d95a67ce056ca9b20a7299909338f1e3cab55
Thom Page <thom.page.000@gmail.com>
2019-06-20 15:05:17 -0400

    Remove duplicate code

--- extract-data.py
+++ extract-data.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from dotenv import load_dotenv
 import pandas as pd
 import math
+import ast
 import sys
 import os
 load_dotenv()
@@ -10,42 +11,44 @@ load_dotenv()
 def main():
     env_flag_list = ['--dev', '--test', '--prod']
     output_flag_list = ['--print', '--count', '--csv']
+    query = sys.argv[3] if len(sys.argv) >= 4 else "{}"
+    fields = sys.argv[4] if len(sys.argv) >= 5 else None
     return (
         print_args_error('Missing arguments') if len(sys.argv) < 3
         else print_args_error('Incorrect env arguments') if not sys.argv[1] in env_flag_list
         else print_args_error('Incorrect output arguments') if not sys.argv[2] in output_flag_list
         else print_args_error('Cannot print or save prod data') if sys.argv[1] == '--prod' and sys.argv[2] != '--count'
-        else set_connection(sys.argv[1], sys.argv[2]))
+        else set_connection(sys.argv[1], sys.argv[2], query, fields))
 
-def set_connection(env_flag, outpt_flag):
+def set_connection(env_flag, output_flag, query, fields):
     return (
         connect_to_db(
             os.getenv('DEV_DB_CONNECTION'), os.getenv('DEV_DB_NAME'), 
-            os.getenv('DEV_DB_COLLECTION'), env_flag, outpt_flag) if env_flag == '--dev'
+            os.getenv('DEV_DB_COLLECTION'), env_flag, output_flag, query, fields) if env_flag == '--dev'
         else connect_to_db(
             os.getenv('TEST_DB_CONNECTION'), os.getenv('TEST_DB_NAME'), 
-            os.getenv('TEST_DB_COLLECTION'), env_flag, outpt_flag) if env_flag == '--test'
+            os.getenv('TEST_DB_COLLECTION'), env_flag, output_flag, query, fields) if env_flag == '--test'
         else connect_to_db(
             os.getenv('PROD_DB_CONNECTION'), os.getenv('PROD_DB_NAME'), 
-            os.getenv('PROD_DB_COLLECTION'), env_flag, outpt_flag) if env_flag == '--prod'
+            os.getenv('PROD_DB_COLLECTION'), env_flag, output_flag, query, fields) if env_flag == '--prod'
         else print_args_error('Missing or incorrect env arguments'))
 
-def connect_to_db(connection_string, db_name, collection_name, env_flag, output_flag):
+def connect_to_db(connection_string, db_name, collection_name, env_flag, output_flag, query, fields):
     connection = MongoClient(connection_string)
     db = connection[db_name][collection_name]
     return (
-        count_documents(connection, db) if output_flag == '--count'
-        else extract_data(connection, db, env_flag, output_flag))
+        count_results(connection, db, query) if output_flag == '--count'
+        else extract_and_process(connection, db, env_flag, output_flag, query, fields))
 
-def count_documents(connection, db):
-    print('Total number of documents in collection: ')
-    print(db.count_documents({}))
+def count_results(connection, db, query):
+    print(db.count_documents( ast.literal_eval(query) ))
     return connection.close()
 
-def extract_data(connection, db, env_flag, output_flag):
-    results = db.find({})
-    # example query to filter (by boolean):
-    # results = db.find({ "field1": True })
+def extract_and_process(connection, db, env_flag, output_flag, query, fields):
+    if not fields:
+        results = db.find(ast.literal_eval(query))
+    else:
+        results = db.find(ast.literal_eval(query), ast.literal_eval(fields)) 
     dataframe = pd.DataFrame(list(results))
     if '_id' in dataframe: del dataframe['_id']
     if '__v' in dataframe: del dataframe['__v']
@@ -55,22 +58,9 @@ def extract_data(connection, db, env_flag, output_flag):
         else print_args_error('Missing or incorrect output format'))
 
 def print_results(connection, dataframe):
-    # restrict columns:
-    # dataframe_selection = dataframe[[ 'field1', 'field2', ... ]]
+    print('=======')
     print(dataframe)
-    return connection.close()
-
-def create_csv(connection, dataframe, env_flag):
-    # restrict columns:
-    # dataframe_selection = dataframe[[ 'field1', 'field2', ... ]]
-    path = './'
-    timestamp = datetime.now().isoformat()
-    filestring = f'{timestamp}{env_flag}.csv'
-    print('Creating csv ...')
-    dataframe.to_csv(path + filestring, index=False, encoding='utf-8')
-    print('Done')
-    print(f'Extracted {len(dataframe.index)} documents to: {path}')
-    print(f'{filestring}')
+    print('=======')
     return connection.close()
 
 def create_csv(connection, dataframe, env_flag):
@@ -112,8 +102,7 @@ def print_args_error(message):
     print('--test')
     print('--prod')
     print('Second argument: output format')
+    print('--print (for --dev and --test only)')
     print('--count')
-    print('--print (not for prod)')
-    print('--csv (not for prod)')
+    print('--csv')
 
-main()
